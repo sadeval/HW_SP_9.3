f5e75caf3d7cf13906d717eeca217cea306e6148
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-05 12:05:38 +0300

    Добавьте файлы проекта.

---
+++ TextUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace TextUtilsLibrary
+{
+    public static class TextUtils
+    {
+        public static bool IsPalindrome(string text)
+        {
+            string cleanedText = new string(text.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+            return cleanedText.SequenceEqual(cleanedText.Reverse());
+        }
+
+        public static int CountSentences(string text)
+        {
+            char[] sentenceEndings = { '.', '!', '?' };
+            return text.Split(sentenceEndings, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string ReverseString(string text)
+        {
+            return new string(text.Reverse().ToArray());
+        }
+    }
+}
